f91e089ad36f7525ef190432aefeeddaa8ab4d79
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2020-03-31 13:27:35 -0400

    final

--- main.js
+++ main.js
@@ -1,64 +1,55 @@
 // javascript document
 
-// access the jason file using an xhr object
+// this is the url of the json file on the server 
+let url = 'https://jeshan26.github.io/Car/car.json';
 
-// This is the function in which XHR Object is being created 
-function loadCar(url, type, callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = type;
 
-    xhr.onload = function() {
-        callback(xhr.response);
-    };
 
-    xhr.send();
-}
+//  this is the fetchh api using promises to fetch the data of the json from server 
+fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObj) {
 
-// below is the callback function which is being called 
+        let carTypes = jsonObj.carTypes;
+        let section = document.querySelector("section");
 
-function car(jsonObj) {
+        for (let i = 0; i < carTypes.length; i++) {
 
-    let carTypes = jsonObj.carTypes;
-    let section = document.querySelector("section");
+            // build html eleements dynamically
+            let article = document.createElement("article");
 
-    for (let i = 0; i < carTypes.length; i++) {
+            let h2 = document.createElement("h2");
+            let image = document.createElement("img");
+            let p1 = document.createElement("p1");
+            let p2 = document.createElement("p2");
+            let ul = document.createElement("ul");
+            // img element is being created for every image of car by passing the actaul source of the image
+            image.setAttribute('src', 'https://jeshan26.github.io/Cars/pics/' + carTypes[i].image);
 
-        // build html eleements dynamically
-        let article = document.createElement("article");
+            image.setAttribute('alt', carTypes[i].image);
+            console.log(image);
 
-        let h2 = document.createElement("h2");
-        let image = document.createElement("img");
-        let p1 = document.createElement("p1");
-        let p2 = document.createElement("p2");
-        let ul = document.createElement("ul");
-        // img element is being created for every image of car by passing the actaul source of the image
-        image.setAttribute('src', 'https://jeshan26.github.io/Cars/pics/' + carTypes[i].image);
+            h2.textContent = carTypes[i].name;
+            p1.textContent = 'Price :' + carTypes[i].price + '\n';
+            p2.textContent = '\n Capacity :' + carTypes[i].capacity;
+            let featuress = carTypes[i].features;
 
-        image.setAttribute('alt', carTypes[i].image);
-        console.log(image);
+            // looping as features contains array
 
-        h2.textContent = carTypes[i].name;
-        p1.textContent = 'Price :' + carTypes[i].price + '\n';
-        p2.textContent = '\n Capacity :' + carTypes[i].capacity;
-        let featuress = carTypes[i].features;
+            for (let j = 0; j < featuress.length; j++) {
+                let listitem = document.createElement('li');
+                listitem.textContent = featuress[j];
+                ul.appendChild(listitem);
+            }
+            // appending the elements in article and then article to section so that everthing is contained in a container
+            article.appendChild(image);
+            article.appendChild(h2);
+            article.appendChild(p1);
+            article.appendChild(p2);
+            article.appendChild(ul);
 
-        // looping as features contains array
-
-        for (let j = 0; j < featuress.length; j++) {
-            let listitem = document.createElement('li');
-            listitem.textContent = featuress[j];
-            ul.appendChild(listitem);
+            section.appendChild(article);
         }
-        // appending the elements in article and then article to section so that everthing is contained in a container
-        article.appendChild(image);
-        article.appendChild(h2);
-        article.appendChild(p1);
-        article.appendChild(p2);
-        article.appendChild(ul);
-
-        section.appendChild(article);
-    }
-}
-//  this is the function being ivoked 
-loadCar('https://jeshan26.github.io/Car/car.json', 'json', car);
+    });
